61261f897642254e974c5694b7e399c3be6bdf72
haripriya2413 <143806953+haripriya2413@users.noreply.github.com>
2024-07-03 14:13:52 +0530

    Update Output.tf

--- Output.tf
+++ Output.tf
@@ -1,6 +1,9 @@
-output "aws_instance_public_ip" {
-  value = aws_route53_record.strapi.records
-
-
+output "alb_dns_name" {
+  description = "The DNS name of the ALB"
+  value       = aws_lb.strapiLB.dns_name
 }
 
+output "ecs_service_name" {
+  description = "The name of the ECS service"
+  value       = aws_ecs_service.nodeapp.name
+}
